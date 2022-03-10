25825a6565044721c7b161b9d8a54a40694fdcbf
UncleBud87 <lperioux@gmail.com>
2022-03-10 16:03:32 -0600

    commit_1

---
+++ create-react-app/big-inversion/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import People from './componesnts/people';
+
+function App() {
+  return (
+    <div className="App">
+      <People firstName = {"Jane"} lastName = {"Doe"} age = {45} hairColor = {"Black"}></People>
+      <People firstName = {"John"} lastName = {"Smith"} age = {88} hairColor = {"Brown"}></People>
+      <People firstName = {"Millard"} lastName = {"Fillmore"} age = {50} hairColor = {"Brown"}></People>
+      <People firstName = {"Maria"} lastName = {"Smith"} age = {62} hairColor = {"Brown"}></People>
+    </div>
+  );
+}
+
+export default App;
