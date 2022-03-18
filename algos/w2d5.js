dcfdb47ee98df092384e15bf9e37e040260c346f
UncleBud87 <lperioux@gmail.com>
2022-03-18 10:16:52 -0500

    commit-1

--- algos/w2d5.js
+++ algos/w2d5.js
@@ -21,3 +21,30 @@ function dedupeString(str){
 }
 
 console.log(dedupeString("sweet potato")) //"swet poa"
+
+
+/*
+
+input: "sweet potato"
+
+output: "swet poa"
+
+*/
+
+
+function dedupeString(str){
+    let newSTR = ""
+for(let i = 0;i < str.length;i++){
+    let duplicate = false
+    for(x = 0;x < i ;x++){
+        if(str[i] == str[x]){
+            duplicate = true
+        }
+    }
+    duplicate == false?newSTR = newSTR + str[i]:null
+}
+return newSTR
+}
+
+
+console.log(dedupeString("sweet potato")) //"swet poa"
