90aa5614c6ba1ca4b2835fcc6439116cf5fd3347
UncleBud87 <lperioux@gmail.com>
2022-03-21 10:12:59 -0500

    commit-1

--- algos/w3d1.js
+++ algos/w3d1.js
@@ -14,3 +14,91 @@
 // Ex: given [2,4,7,9,10] and [2,3,5,7,9,10], return [2,3,4,5,7,9,10]
 // Ex: given [1,2,2,2,7] and [2,2,6,6,7] return [1,2,2,2,6,6,7]
 // Ex: given [1,5,9] and [2,6,10] return [1,2,5,6,9,10]
+const union = (arr1, arr2) => {
+    let output = [];
+    let a2 = [...arr2];
+    arr1.forEach(x => {
+        for (let i = 0; i < a2.length; i++) {
+            if (a2[i] === x) {
+                output.push(x);
+                a2 = a2.splice(i + 1);
+                break;
+            }
+        }
+    });
+    return output;
+}
+
+const unionSort = (arr1, arr2) => {
+    let output = [];
+    let a2 = [...arr2];
+    arr1.forEach(x => {
+        while (a2[0] < x) {
+            output.push(a2.shift());
+        }
+        if (a2[0] === x) {
+            output.push(a2.shift());
+        } else {
+            output.push(x);
+        }
+    });
+    return output;
+}
+
+const unionSort2_OldSchoolBoogaloo = (arr1, arr2) => {
+    let storage = Array(arr1.length + arr2.length);
+    let [x, y] = [0, 0];
+    let length = 0;
+    for (let i = 0; x < arr1.length || y < arr2.length; i++){
+        if (arr1[x] === arr2[y]){
+            storage[i] = arr1[x];
+            x++;
+            y++
+        } else if (arr1[x] < arr2[y]){
+            storage[i] = arr1[x];
+            x++;
+        } else {
+            storage[i] = arr2[y];
+            y++;
+        }
+        length++;
+    }
+    let output = Array(length);
+    for (let i = 0; i < length; i++){
+        output[i] = storage[i];
+    }
+    return output;
+}
+
+const intersect = (arr1, arr2) => {
+    const result = [];
+    let [x,y] = [0,0];
+    while (x < arr1.length && y < arr2.length) {
+        if (arr1[x] < arr2[y]) x++;
+        if (arr1[x] > arr2[y]) y++;
+        if (arr1[x]===arr2[y])
+        {result[result.length] = arr1[x], x,y++}
+    }return result;
+}
+let arr1 = [1, 2, 2, 3, 4];
+let arr2 = [2, 2, 4, 6, 7, 8];
+console.log(intersect(arr1, arr2));
+
+
+
+
+
+const unionSortedArrays = (arr1, arr2) => {
+    const result = [];
+    let [i,j] = [0,0];
+        while ( i < arr1.length || j < arr2.length){
+            if (arr1[i] < arr2[j] || !arr2[j]){
+                result[result.length] = arr1[i];
+                i++;
+            }else{
+                result[result.length] = arr2[j];
+                j++;
+            }
+        }return result;
+}
+console.log(unionSortedArrays([0,3,4,31], [4,6,30]));
