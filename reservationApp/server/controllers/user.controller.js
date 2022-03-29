0e93f3bf15b2f7f856cf175eb7ae41db6c741eb6
UncleBud87 <lperioux@gmail.com>
2022-03-29 17:57:13 -0500

    commit-1

--- reservationApp/server/controllers/user.controller.js
+++ reservationApp/server/controllers/user.controller.js
@@ -25,98 +25,75 @@ class UserController {
                                 .json({ msg: "success!", user: user });
                         })
                         .catch(err => res.json(err));
-                }else{
-                    res.json({emailTaken:'Email is already in use'})
+                } else {
+                    res.json({ errors: { email: { message: 'Email is already in use' } } })
                 }
             })
             .catch(err => console.log('err', err))
-
-        login = async (req, res) => {
-            const user = await User.findOne({ email: req.body.email });
-            if (user === null) {
-                return res.json({ msg: "User not found." })
-            }
-
-            const correctPassword = await bcrypt.compare(req.body.password, user.password);
-            if (!correctPassword) {
-                return res.sendStatus(400);
-            }
-
-            const userToken = jwt.sign({
-                id: user._id
-            }, process.env.SECRET_KEY);
-
-            res
-                .cookie("usertoken", userToken, secret, {
-                    httpOnly: true
-                })
-                .json({ msg: "success!" });
-        }
-
-        logout = (req, res) => {
-            res.clearCookie('usertoken');
-            res.sendStatus(200);
-        }
-
-        findAllUsers = (req, res) => {
-            User.find()
-                .then(allUsers => {
-                    res.json({ results: allUsers })
-                })
-                .catch(err => res.json({ message: 'Something went wrong', error: err }))
-        }
-
-        findOneUser = (req, res) => {
-            User.findOne({ _id: req.params.id })
-                .then(oneUser => {
-                    res.json({ results: oneUser })
-                })
-                .catch(err => res.json({ message: 'Something went wrong', error: err }))
-        }
-
-        updateOneUser = (req, res) => {
-            User.findOneAndUpdate({ _id: req.params.id },
-                req.body,
-                { new: true, runValidators: true }
-            )
-                .then(updatedUser => {
-                    res.json({ results: updatedUser })
-                })
-                .catch(err => res.json({ message: 'Something went wrong', error: err }))
-        }
-
-        deleteOneUser = (req, res) => {
-            User.deleteOne({ _id: req.params.id })
-                .then(deleteUser => {
-                    res.json({ results: deleteUser })
-                })
-                .catch(err => res.json({ message: 'Something went wrong', error: err }))
-        }
-
     }
 
+    login = async (req, res) => {
+        const user = await User.findOne({ email: req.body.email });
+        if (user === null) {
+            return res.json({ msg: "User not found." })
+        }
+
+        const correctPassword = await bcrypt.compare(req.body.password, user.password);
+        if (!correctPassword) {
+            return res.sendStatus(400);
+        }
+
+        const userToken = jwt.sign({
+            id: user._id
+        }, process.env.SECRET_KEY);
+
+        res
+            .cookie("usertoken", userToken, secret, {
+                httpOnly: true
+            })
+            .json({ msg: "success!" });
+    }
+
+    logout = (req, res) => {
+        res.clearCookie('usertoken');
+        res.sendStatus(200);
+    }
+
+    findAllUsers = (req, res) => {
+        User.find()
+            .then(allUsers => {
+                res.json({ results: allUsers })
+            })
+            .catch(err => res.json({ message: 'Something went wrong', error: err }))
+    }
+
+    findOneUser = (req, res) => {
+        User.findOne({ _id: req.params.id })
+            .then(oneUser => {
+                res.json({ results: oneUser })
+            })
+            .catch(err => res.json({ message: 'Something went wrong', error: err }))
+    }
+
+    updateOneUser = (req, res) => {
+        User.findOneAndUpdate({ _id: req.params.id },
+            req.body,
+            { new: true, runValidators: true }
+        )
+            .then(updatedUser => {
+                res.json({ results: updatedUser })
+            })
+            .catch(err => res.json({ message: 'Something went wrong', error: err }))
+    }
+
+    deleteOneUser = (req, res) => {
+        User.deleteOne({ _id: req.params.id })
+            .then(deleteUser => {
+                res.json({ results: deleteUser })
+            })
+            .catch(err => res.json({ message: 'Something went wrong', error: err }))
+    }
+
+}
+
 module.exports = new UserController();
-
-
-
-
-    // login = (req, res) => {
-    //     User.findOne({ email: req.body.email })
-    //         .then(user => {
-    //             if (user === null) {
-    //                 res.json({ msg: "invalid login attempt-user not found" })
-    //             } else {
-    //                 bcrypt.compare(req.body.password, user.password)
-    //                     .then(passwordIsValid => {
-    //                         if (passwordIsValid) {
-    //                             res.cookie("usertoken", jwt.sign({ _id: user._id }, process.env.SECRET_KEY), { httpOnly: true })
-    //                                 .json({ msg: "success!" });
-    //                         } else {
-    //                             res.json({ msg: "invalid login attempt- password incorrect" })
-    //                         }
-    //                     })
-    //                     .catch(err => res.json({ msg: "invalid login attempt", err }))
-    //             }
-    //         })
-    //         .catch(err => res.json(err))
-    // }
