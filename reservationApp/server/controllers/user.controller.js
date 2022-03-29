210f9c34338089e05ceafb8471e846af30698400
UncleBud87 <lperioux@gmail.com>
2022-03-29 15:07:35 -0500

    commit-1

--- reservationApp/server/controllers/user.controller.js
+++ reservationApp/server/controllers/user.controller.js
@@ -1,24 +1,10 @@
 const User = require('../models/user.model');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 
 
 class UserController {
-
-    findAllUsers = (req, res) => {
-        User.find()
-            .then(allUsers => {
-                res.json({ results: allUsers })
-            })
-            .catch(err => res.json({ message: 'Something went wrong', error: err }))
-    }
-
-    findOneUser = (req, res) => {
-        User.findOne({ _id: req.params.id })
-            .then(oneUser => {
-                res.json({ results: oneUser })
-            })
-            .catch(err => res.json({ message: 'Something went wrong', error: err }))
-    }
 
     registerUser = (req, res) => {
         User.create(req.body)
@@ -28,7 +14,7 @@ class UserController {
                 }, process.env.SECRET_KEY);
 
                 res
-                    .cookie("usertoken", userToken, secret, {
+                    .cookie("usertoken", userToken, process.env.SECRET_KEY, {
                         httpOnly: true
                     })
                     .json({ msg: "success!", user: user });
@@ -39,7 +25,7 @@ class UserController {
     login = async (req, res) => {
         const user = await User.findOne({ email: req.body.email });
         if (user === null) {
-            return res.sendStatus(400);
+            return res.json({msg: "User not found."})
         }
 
         const correctPassword = await bcrypt.compare(req.body.password, user.password);
@@ -58,7 +44,26 @@ class UserController {
             .json({ msg: "success!" });
     }
 
+    logout = (req, res) => {
+        res.clearCookie('usertoken');
+        res.sendStatus(200);
+    }
 
+    findAllUsers = (req, res) => {
+        User.find()
+            .then(allUsers => {
+                res.json({ results: allUsers })
+            })
+            .catch(err => res.json({ message: 'Something went wrong', error: err }))
+    }
+
+    findOneUser = (req, res) => {
+        User.findOne({ _id: req.params.id })
+            .then(oneUser => {
+                res.json({ results: oneUser })
+            })
+            .catch(err => res.json({ message: 'Something went wrong', error: err }))
+    }
 
     updateOneUser = (req, res) => {
         User.findOneAndUpdate({ _id: req.params.id },
@@ -83,3 +88,26 @@ class UserController {
 
 module.exports = new UserController();
 
+
+
+
+    // login = (req, res) => {
+    //     User.findOne({ email: req.body.email })
+    //         .then(user => {
+    //             if (user === null) {
+    //                 res.json({ msg: "invalid login attempt-user not found" })
+    //             } else {
+    //                 bcrypt.compare(req.body.password, user.password)
+    //                     .then(passwordIsValid => {
+    //                         if (passwordIsValid) {
+    //                             res.cookie("usertoken", jwt.sign({ _id: user._id }, process.env.SECRET_KEY), { httpOnly: true })
+    //                                 .json({ msg: "success!" });
+    //                         } else {
+    //                             res.json({ msg: "invalid login attempt- password incorrect" })
+    //                         }
+    //                     })
+    //                     .catch(err => res.json({ msg: "invalid login attempt", err }))
+    //             }
+    //         })
+    //         .catch(err => res.json(err))
+    // }
